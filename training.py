35f92ee9e1839589e923d4bd11b452b4207127f6
franciscolagos <f.bunout01@ufromail.cl>
2019-07-27 23:06:45 -0400

    generacion de modelo de entrenamiento apartir de diversas imagenes

--- training.py
+++ training.py
@@ -1,1 +1,51 @@
-#comming soon
+import os,cv2
+from PIL import Image
+import numpy as np
+import pickle
+
+BASE_DIR =os.path.dirname(os.path.abspath(__file__))
+
+image_dir = os.path.join(BASE_DIR,"imagenes")
+face_cascade = cv2.CascadeClassifier('cascade/data/haarcascade_frontalface_alt2.xml')
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+
+current_id = 0
+label_ids = {}
+y_labels = []
+x_train = []
+
+for root, dir, files in os.walk(image_dir):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpg"):
+            path = os.path.join(root, file)
+            label = os.path.basename(os.path.dirname(path)).replace(" ","-").lower()
+            #print(label,path)
+            if not label in label_ids:
+                label_ids[label] = current_id
+                current_id += 1
+
+            id_ = label_ids[label]
+            #print(label_ids)
+            #convierte la imagen a grices
+            pil_image = Image.open(path).convert("L")  # grayscale
+            size = (550, 550)
+            final_image = pil_image.resize(size, Image.ANTIALIAS)
+            image_array = np.array(final_image, "uint8")
+
+            faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+            for (x, y, w, h) in faces:
+                roi = image_array[y:y+h, x:x+w]
+                x_train.append(roi)
+                y_labels.append(id_)
+
+print(y_labels)
+print(x_train)
+
+
+with open("face-labels.pickle", 'wb') as f:
+	pickle.dump(label_ids, f)
+
+recognizer.train(x_train, np.array(y_labels))
+recognizer.save("face-trainner.yml")
